21e1c624dee30aff313e006f93280952d1d92acc
rafaelrandow <rafaelrandow@gmail.com>
2023-11-16 09:13:32 -0300

    titulo e badges

--- App.tsx
+++ App.tsx
@@ -1,36 +1,38 @@
 import { NativeBaseProvider, StatusBar, HStack, Image } from 'native-base';
 import { Titulo } from "./src/componentes/Titulo";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { TEMAS } from './src/estilos/temas';
 import Logo from './src/assets/alesp.png';
 import Rotas from './src/Rotas';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator()
 const screenOptions = {
   tabBarStyle: {
-    height:66,
+    height:70,
     backgroundColor: '#0B3B60',
   },
-  tabBarActiveTintColor: "transparent",
+  tabBarActiveTintColor: "transparent",  
+  tabBarInActiveTintColor: "transparent",
 }
 
+let numNotifica = 3;
 
 export default function App() {
   return (
     <NativeBaseProvider theme={TEMAS}>
       <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
       <HStack bg={TEMAS.colors.blue[800]} w="100%" px={5} alignItems={"center"}>  
-      <Image source={Logo} alt="Logo" style={{ width: 66, height: 66, resizeMode: 'contain' }} />
-      <Titulo color="white" pr={1} w="66%">Alesp Mobile</Titulo>      
       <NavigationContainer >
+      <Image source={Logo} alt="Logo" style={{ width: 70, height: 70, resizeMode: 'contain' }} />
+      <Titulo color="white" pr={10} w="280">Alesp Mobile</Titulo>      
       <Tab.Navigator screenOptions={screenOptions}>
         <Tab.Screen 
           name='notification' 
           component={Titulo}
           options={{
-            tabBarBadge: 3,
+            tabBarBadge: numNotifica,
             headerShown: false,
             tabBarShowLabel:false,
             tabBarIcon: () => (
